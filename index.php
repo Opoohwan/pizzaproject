96ea48925f4fe395b9ac1dd0310075f630f0247d
Opoohwan <dnclyon@gmail.com>
2017-02-04 19:41:14 -0700

    start to build barebones site

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-Test site
+<?php require('partials/header.php'); ?>
+<div class="container-fluid">
+	<div class="row">
+		<div class="main">
+			<h1 class="page-header">Pizza Dashboard</h1>
+			<div class="row placeholders">
+				<div class="col-xs-6 col-sm-3 placeholder">
+					<a href="pizza.php">
+						<img src="images/pizza.jpg" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+						<h4>Pizzas</h4>
+						<span class="text-muted">View/Create/Edit</span>
+					</a>
+				</div>
+				<div class="col-xs-6 col-sm-3 placeholder">
+					<a href="topping.php">
+						<img src="images/pepperoni.jpg" width="200" height="200" class="img-responsive" alt="Generic placeholder thumbnail">
+						<h4>Toppings</h4>
+						<span class="text-muted">View/Create</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php require('partials/footer.php'); ?>
